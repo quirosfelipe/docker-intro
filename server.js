fd74be780c72e6af3e3c7c003c43b020faebb750
Felipe Quiros <59105242+quirosfelipe@users.noreply.github.com>
2021-10-18 16:09:48 -0400

    Tests added (#1)
    * tests added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require("http");
-const { readFile } = require("fs").promises;
+const fs = require("fs");
 
-const server = http.createServer(async (req, res) => {
+const server = http.createServer(function (req, res) {
   if (req.method === "GET" && req.url === "/") {
     const homeFilePath = "./helloworld.html";
-    const homeFileContents = await readFile(homeFilePath);
+    const homeFileContents =  fs.readFileSync(homeFilePath);
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.end(homeFileContents);
@@ -21,3 +21,8 @@ const port = 8081;
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+server.close((err) => {
+  console.log('server closed')
+  process.exit(err ? 1 : 0)
+})
---
+++ test/test.js
@@ -0,0 +1,47 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+var server = require('../server.js');
+
+describe('server', function () {
+  before(function () {
+    server.listen(8081);
+  });
+
+  after(function () {
+    server.close();
+  });
+});
+
+var assert = require('assert'),
+    http = require('http');
+
+describe('/', function () {
+  it('should return 200', function (done) {
+    http.get('http://localhost:8081', function (res) {
+      assert.equal(200, res.statusCode);
+      done();
+    });
+  });
+
+  it('should say "Hello, world!"', function (done) {
+    http.get('http://localhost:8081', function (res) {
+      var data = '';
+
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+
+      res.on('end', function () {
+        assert.equal('Hello, world!', data);
+        done();
+      });
+    });
+  });
+});
